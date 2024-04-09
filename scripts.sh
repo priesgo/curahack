c1f099cfad4af138ddff77d82e0a06f9eb4c1f0b
Pablo Riesgo-Ferreiro <priesgoferreiro@gmail.com>
2024-04-09 11:26:55 +0200

    update script to consider single-end reads + update filtering results

--- scripts.sh
+++ scripts.sh
@@ -2,31 +2,49 @@
 fastqc downsampled_fastq/*.fastq.gz
 
 # runs Trimmomatic on input FASTQs cropping to 72 bp, min length 72 bp and sliding window of 2 bp below 32 Phred score
-trimmomatic PE \
+trimmomatic SE \
 	downsampled_fastq/Control_siRNA_1.fastq.gz \
-	downsampled_fastq/Control_siRNA_2.fastq.gz \
 	trimmed_fastq/Control_siRNA_1.trimmomatic.fastq.gz \
-	trimmed_fastq/Control_siRNA_1u.trimmomatic.fastq.gz \
+	CROP:72 MINLEN:72 SLIDINGWINDOW:32:2
+
+trimmomatic SE \
+	downsampled_fastq/Control_siRNA_2.fastq.gz \
 	trimmed_fastq/Control_siRNA_2.trimmomatic.fastq.gz \
-	trimmed_fastq/Control_siRNA_2u.trimmomatic.fastq.gz \
 	CROP:72 MINLEN:72 SLIDINGWINDOW:32:2
 
-trimmomatic PE \
+trimmomatic SE \
+	downsampled_fastq/Control_siRNA_3.fastq.gz \
+	trimmed_fastq/Control_siRNA_3.trimmomatic.fastq.gz \
+	CROP:72 MINLEN:72 SLIDINGWINDOW:32:2
+
+trimmomatic SE \
 	downsampled_fastq/STAT5A_siRNA_1.fastq.gz \
-	downsampled_fastq/STAT5A_siRNA_2.fastq.gz \
-	trimmed_fastq/STAT5A_1.trimmomatic.fastq.gz \
-	trimmed_fastq/STAT5A_1u.trimmomatic.fastq.gz \
-	trimmed_fastq/STAT5A_2.trimmomatic.fastq.gz \
-	trimmed_fastq/STAT5A_2u.trimmomatic.fastq.gz \
+	trimmed_fastq/STAT5A_siRNA_1.trimmomatic.fastq.gz \
 	CROP:72 MINLEN:72 SLIDINGWINDOW:32:2
 
-trimmomatic PE \
+trimmomatic SE \
+	downsampled_fastq/STAT5A_siRNA_2.fastq.gz \
+	trimmed_fastq/STAT5A_siRNA_2.trimmomatic.fastq.gz \
+	CROP:72 MINLEN:72 SLIDINGWINDOW:32:2
+
+trimmomatic SE \
+	downsampled_fastq/STAT5A_siRNA_3.fastq.gz \
+	trimmed_fastq/STAT5A_siRNA_3.trimmomatic.fastq.gz \
+	CROP:72 MINLEN:72 SLIDINGWINDOW:32:2
+
+trimmomatic SE \
 	downsampled_fastq/STAT5B_siRNA_1.fastq.gz \
+	trimmed_fastq/STAT5B_siRNA_1.trimmomatic.fastq.gz \
+	CROP:72 MINLEN:72 SLIDINGWINDOW:32:2
+
+trimmomatic SE \
 	downsampled_fastq/STAT5B_siRNA_2.fastq.gz \
-	trimmed_fastq/STAT5B_1.trimmomatic.fastq.gz \
-	trimmed_fastq/STAT5B_1u.trimmomatic.fastq.gz \
-	trimmed_fastq/STAT5B_2.trimmomatic.fastq.gz \
-	trimmed_fastq/STAT5B_2u.trimmomatic.fastq.gz \
+	trimmed_fastq/STAT5B_siRNA_2.trimmomatic.fastq.gz \
+	CROP:72 MINLEN:72 SLIDINGWINDOW:32:2
+
+trimmomatic SE \
+	downsampled_fastq/STAT5B_siRNA_3.fastq.gz \
+	trimmed_fastq/STAT5B_siRNA_3.trimmomatic.fastq.gz \
 	CROP:72 MINLEN:72 SLIDINGWINDOW:32:2
 
 # runs QC again after trimming and filters
